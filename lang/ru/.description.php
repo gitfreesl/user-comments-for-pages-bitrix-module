c2485bf67d1d51e377a31a2b17d01cf3adf3783d
gitfreesl <msl-info@yandex.by>
2017-07-28 16:02:55 +0300

    Add files via upload

---
+++ lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS ['USER_COMMENTS_COMPONENT_NAME'] = "Комментарии";
+$MESS ['USER_COMMENTS_COMPONENT_DESCR'] = "Комментарии для страницы и елементов ИБ";
+?>
---
+++ lang/ru/.parameters.php
@@ -0,0 +1,7 @@
+<?
+$MESS ['MFP_TYPE'] = "Тип объекта";
+$MESS ['MFP_OBJECT'] = "Объект комментирования";
+$MESS ['MFP_TYPE_ELEMENT'] = "Элемент ИБ";
+$MESS ['MFP_TYPE_PAGE'] = "Страница";
+
+?>
